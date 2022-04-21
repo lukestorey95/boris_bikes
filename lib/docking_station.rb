3e78855354c8d99d693cbf1cf89d359612aaa3b5
Archie <archieparchie@Archies-MacBook-Air.local>
2022-04-21 17:56:39 +0100

    added initialize, edited dock function

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,22 @@
 class DockingStation
 
+  attr_reader :bike
+  
+  def initialize
+    @bike = []
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
   end
 
   def dock(bike)
-    fail 'Dock is full' if @bike
-    @bike = bike
+    fail 'Dock is full' if @bike.count == 20
+    @bike << bike
   end
 
-  attr_reader :bike
+  
 end
 
 class Bike
