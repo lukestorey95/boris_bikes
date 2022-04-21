5bd10840ff06b3c04e348b6d51d8b5ab20318a3b
alexandrospapagiannis <alpa31291@gmail.com>
2022-04-21 14:58:58 +0100

    Step 10 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,15 @@ require 'docking_station'
 describe DockingStation do
 it 'responds to the method release_bike' do
   bike = DockingStation.new
-  expect(bike.release_bike).to eq nil
+  expect(bike.release_bike).to be_truthy
 end
+it 'gets a bike and tests if it is working' do
+  station = DockingStation.new
+  bike = station.release_bike
+  working_bike = bike.working?
+  expect(bike).to be_truthy
 end
 
 # it "shows the condition of a bike as working or not" do 
 
-# end 
+end 
