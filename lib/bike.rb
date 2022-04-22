d0d24c3d3d92050efa6f33094a9de0630742e63b
alexandrospapagiannis <alpa31291@gmail.com>
2022-04-22 18:40:06 +0100

    Reporting broken bikes

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,14 @@
 class Bike
+
+  def initialize
+    @bike_condition = true
+  end
+
   def working?
-      
+    @bike_condition
+  end
+
+  def report_broken
+    @bike_condition = false
   end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,26 @@
 describe Bike do
-  it 'asks the bike if it is working' do
-  bike = Bike.new
-  expect(bike).to respond_to(:working?)
+  describe '#working?' do
+    it 'asks the bike if it is working' do
+      bike = Bike.new
+      expect(bike).to respond_to(:working?)
+    end
+
+    it 'checks if the bike is working' do
+      expect(subject.working?).to be_truthy  
+    end
+
+    context 'when the bike is reported as broken' do
+      it 'should return false' do
+        subject.report_broken
+        expect(subject.working?).to be_falsey
+      end  
+    end
+  end
+    
+  describe '#report_broken' do
+    it 'reports that the bike is broken' do
+      bike = Bike.new
+      expect(bike).to respond_to(:report_broken)
+    end
   end
 end
